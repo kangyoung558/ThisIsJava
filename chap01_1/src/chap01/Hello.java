a6a05785b03d97e325799e70d57f9d5128bc1959
kangyoung558 <kangyoung558@gmail.com>
2021-01-14 16:44:58 +0900

    frist commit

---
+++ chap01_1/src/chap01/Hello.java
@@ -0,0 +1,19 @@
+package chap01;
+/**
+ * 
+ * @author dlrkd
+ *@
+ */
+public class Hello {
+	public static void main(String[] args) { 
+		System.out.println("Hello World"); //syso ctrl + space
+		
+		//����Ű
+		//ctrl + D 삭제
+		// alt + 위아래 화살표: 이동 
+		// alt + ctrl + 위아래 방향키 : 복사
+	 
+	}
+	
+	
+}
