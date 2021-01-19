042943006e04ee1d94f50b21bd4c88a66f8d2e58
kangyoung558 <kangyoung558@gmail.com>
2021-01-19 17:32:32 +0900

    Add New Class

--- chap04/src/js/Ex09.java
+++ chap04/src/js/Ex09.java
@@ -5,7 +5,9 @@ public class Ex09 {
 		String str = "12345";
 		int sum = 0;
 		for (int i = 0; i < str.length(); i++) {
-			sum += str.charAt(i) - '0';
+			sum+= str.charAt(i) -48;
+			//아스키 코드 표 
+			// 49-48 + 50-48 + 51-48 + 52-48 + 53-48 
 		}
 		System.out.println("sum = " + sum);
 	}
--- chap04/src/js/Ex15.java
+++ chap04/src/js/Ex15.java
@@ -2,21 +2,26 @@ package js;
 
 public class Ex15 {
 	public static void main(String[] args) {
-		int num = 78945;
+		int num = 12321;
 		int tmp = num;
 		int result =0;
 		
 		while(tmp !=0) {
-			result *= 10; //10 키우고 
+			result *= 10; // 자리수 늘림
+			result += tmp%10; //  뒤에꺼 떼서 result에 붙힘
+			// 0 * 10 = 0
+			// 0 + 12321 % 10 = 1
+			// 1 * 10 = 10
+			// 10 + 1232 % 10 = 12
 			
-
-			result += tmp%10; //  뒤에 붙힘
-
-			tmp/=10;
+			tmp/=10; // 다음 자리수를 구하기 위해 필요한 식
+			System.out.println(result);
+			System.out.println(tmp);
 		}
-		if(num == result)
+		if(num == result) {
 		System.out.println( num + "는 회문수 입니다.");
-		else
-		System.out.println( num + "는 회문수가 아닙니다.");
+		} else {
+			System.out.println( num + "는 회문수가 아닙니다.");
+		}
 	}
 }
